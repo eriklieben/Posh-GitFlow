87a706591b2d09b0376b98a4bb91356742edee72
Erik Lieben <erik@codetistic.com>
2015-04-28 23:04:46 +0200

    Add alias

--- Git-Alias.ps1
+++ Git-Alias.ps1
@@ -6,4 +6,8 @@ Set-Alias ff Finish-Feature -scope Global
 
 Set-Alias sr Start-Release -scope Global
 Set-Alias fr Finish-Release -scope Global
+
+Set-Alias shf Start-HotFix -scope Global
+Set-Alias fhf Finish-HotFix -scope Global
+
 Set-Alias igf Init-GitFlow -scope Global
